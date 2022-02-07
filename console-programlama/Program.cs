70130c8ea5851a093a8bb191182291f34d1f021c
Salwitore <93592152+Salwitore@users.noreply.github.com>
2022-02-07 11:07:23 +0300

    Add files via upload

---
+++ console-programlama/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace console_programlama
+{
+
+    class Program
+    {
+
+        public static void Main (string[] args)
+        {
+
+        Console.WriteLine("İsminizi Girin");
+        string name = Console.ReadLine();
+        Console.WriteLine("Soyisminizi Girin");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine("Hoşgeldin" + name + " " + surname);
+
+
+       }
+    }
+}        
