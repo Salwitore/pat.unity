42ad8d6097bb4598ffc5f99eb153491ad4a7f98d
Salwitore <93592152+Salwitore@users.noreply.github.com>
2022-02-13 17:39:40 +0300

    Add files via upload

---
+++ switch-case/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace switch_case{
+    class Program{
+        static void Main(string[] args){
+
+            int month = DateTime.Now.Month;
+
+            // Expression
+            switch (month)
+            {
+                case 1:
+                Console.WriteLine("Ocak Ayındasınız!");
+                break;
+
+                case 2:
+                 Console.WriteLine("Şubat Ayındasınız!");
+                break;
+                case 3:
+                 Console.WriteLine("Mart Ayındasınız!");
+                break;
+                case 4:
+                 Console.WriteLine("Nisan Ayındasınız!");
+                break;
+
+                default:
+                Console.WriteLine("Yanlış veri girişi");
+                break;
+            }
+
+        }
+    }
+}
