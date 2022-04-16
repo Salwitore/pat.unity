82fceb9ad62f06af05361f3084db136b4e51225a
Harun Üst <93592152+Salwitore@users.noreply.github.com>
2022-04-16 14:53:24 +0300

    Add files via upload

---
+++ ParamsKeyWord.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParamsKeyWord : MonoBehaviour
+{
+    [SerializeField] Text text;
+    
+    void Start()
+    {
+        Method(1, 1, 1, 1, 1, 1);
+
+    }
+
+    void Method(params int[] x){ //params keywordu methoda istediğimiz kadar parametre almamızı sağlar.
+
+        int sonuc = 0;
+
+        foreach(var num in x)
+        {
+            sonuc += num;
+        }
+        text.text = sonuc.ToString();
+
+    }
+    
+}
